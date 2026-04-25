6b4b505a1ca9fd60c3989ed8506183792d303704
my-horizon <mail@virtualhorizon.de>
2026-04-25 19:20:39 +0200

    prepare for un change

--- modules/hosts/pi5/pi5.nix
+++ modules/hosts/pi5/pi5.nix
@@ -4,6 +4,7 @@
     system = "aarch64-linux";
     specialArgs = { inherit inputs; };
     modules = [
+      ../../../hardware/pi5/pi5-hardware.nix
       inputs.nixos-raspberrypi.nixosModules.raspberry-pi-5.base
       inputs.nixos-raspberrypi.nixosModules.raspberry-pi-5.page-size-16k
 
